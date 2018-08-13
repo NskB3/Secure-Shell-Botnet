5babeedba2a88c624292a00507eabb0b05f6d407
NskB3 <40596952+NskB3@users.noreply.github.com>
2018-08-13 13:25:41 +0000

    Add files via upload

---
+++ botnet.py
@@ -0,0 +1,52 @@
+from pexpect import pxssh
+class Bot:
+	def __init__(self, host, username, password):
+		self.host = host
+		self.username = username
+		self.password = password
+		self.session = self.ssh()
+	def ssh(self):
+		try:
+			s = pxssh.pxssh()
+			s.login(self.host, self.username, self.password)
+			return s
+		except:
+			print("Connection Failed!")
+	def send_command(self, comm):
+		self.session.sendline(comm)
+		self.session.prompt()
+		return self.session.before
+
+def botnet_command(command):
+	for bot in botnet:
+		output = bot.send_command(command)
+		print bot.host + ":\n"
+		print("\n" + output)
+def add_bot(host, username, password):
+	bot = Bot(host, username, password)
+	botnet.append(bot)
+botnet = []
+def menu():
+	print """
+Welcome To The SSH Botnet!
+Options:
+
+1. Add Bots
+2. Control your existing bots
+
+Enter the number of the chosen option
+"""
+menu()
+option = raw_input(">> ")
+if option == "1":
+	ip = raw_input("Bot IP: ")
+	uname = raw_input("Bot Username: ")
+	passw = raw_input("Bot's SSH Login Password: ")
+	add_bot(ip,uname,passw)
+	while True:
+		commandtosend = raw_input("$ ")
+		botnet_command(commandtosend)
+else:
+	while True:
+		inputtedcommand = raw_input("$ ")
+		botnet_command(inputtedcommand)
